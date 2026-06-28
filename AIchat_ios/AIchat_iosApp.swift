47edd5c15d69a5da336f0fb0a37b1ce822e8713e
chenanqi1024 <chenanqi1024@gmail.com>
2026-06-28 22:00:32 +0800

    Initial Commit

---
+++ AIchat_ios/AIchat_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIchat_iosApp.swift
+//  AIchat_ios
+//
+//  Created by chenanqi on 2026/6/28.
+//
+
+import SwiftUI
+
+@main
+struct AIchat_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
